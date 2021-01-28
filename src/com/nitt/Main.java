1212ce648d6167a846b19d1ec1378c342c067a9e
26kunal <bhavsarkunal2103@gmail.com>
2021-01-29 00:17:00 +0530

    commiting

---
+++ src/com/nitt/Main.java
@@ -0,0 +1,15 @@
+package com.nitt;
+
+
+import com.nitt.model.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        Product product=new Product("kunal");
+        System.out.println(product.getName());
+
+    }
+}
